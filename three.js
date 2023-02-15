ec1de6452303388b102558f4a9f27b7b5d292a67
vover11 <83251234+vover11@users.noreply.github.com>
2023-02-16 01:18:32 +0300

    Add files via upload

--- three.js
+++ three.js
@@ -1,3 +1,8 @@
+import * as THREE from "https://threejsfundamentals.org/threejs/resources/threejs/r127/build/three.module.js";
+import { OrbitControls } from "https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/controls/OrbitControls.js";
+import { GUI } from "https://threejsfundamentals.org/threejs/../3rdparty/dat.gui.module.js";
+
+
 var container3d = document.querySelector('.container3d');
 
 var w = container3d.offsetWidth;
@@ -5,43 +10,235 @@ var h = container3d.offsetHeight;
 
 var scene = new THREE.Scene();
 
-var camera = new THREE.PerspectiveCamera(75, w/h, 0.1,1000);
+var camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.z = 200;
 
 var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.domElement.id = "canvasfirst";
 renderer.domElement.classList.add("canvasfirst");
+const canvas = renderer.domElement;
 container3d.appendChild(renderer.domElement);
-renderer.setSize( w, h);
-renderer.setClearColor( 0xefefef, 1);
-
-var geometry = new THREE.TorusKnotGeometry(50, 6, 500, 16, 3, 2, 2);
-var material = new THREE.MeshBasicMaterial({color: 0x0000ff, wireframe:true}); // set the color of the material to blue (0x0000ff)
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-var light = new THREE.PointLight(0xFFFF00);
-light.position.set(10, 10, 25);
-scene.add(light);
-
-var render = function () {
-requestAnimationFrame(render);
-
-cube.rotation.x += 0.01;
-cube.rotation.y += 0.005;
-cube.rotation.z += 0.05;
-
-renderer.render(scene, camera);
-};
-render();
-
-window.addEventListener('resize', onWindowResize, false);
-
-function onWindowResize() {
-w = container3d.offsetWidth;
-h = container3d.offsetHeight;
-camera.aspect = w / h;
-camera.updateProjectionMatrix();
 renderer.setSize(w, h);
-renderer.setClearColor( 0xefefef, 1);
-}
+renderer.setClearColor(0xefefef, 1);
+
+function main() {
+    
+
+  
+    const fov = 45;
+    const aspect = 2; // the canvas default
+    const near = 0.1;
+    const far = 100;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.set(0, 10, 20);
+  
+    class MinMaxGUIHelper {
+      constructor(obj, minProp, maxProp, minDif) {
+        this.obj = obj;
+        this.minProp = minProp;
+        this.maxProp = maxProp;
+        this.minDif = minDif;
+      }
+      get min() {
+        return this.obj[this.minProp];
+      }
+      set min(v) {
+        this.obj[this.minProp] = v;
+        this.obj[this.maxProp] = Math.max(
+          this.obj[this.maxProp],
+          v + this.minDif
+        );
+      }
+      get max() {
+        return this.obj[this.maxProp];
+      }
+      set max(v) {
+        this.obj[this.maxProp] = v;
+        this.min = this.min; // this will call the min setter
+      }
+    }
+  
+    function updateCamera() {
+      camera.updateProjectionMatrix();
+    }
+  
+    const gui = new GUI();
+    gui.add(camera, "fov", 1, 180).onChange(updateCamera);
+    const minMaxGUIHelper = new MinMaxGUIHelper(camera, "near", "far", 0.1);
+    gui
+      .add(minMaxGUIHelper, "min", 0.1, 50, 0.1)
+      .name("near")
+      .onChange(updateCamera);
+    gui
+      .add(minMaxGUIHelper, "max", 0.1, 50, 0.1)
+      .name("far")
+      .onChange(updateCamera);
+  
+    const controls = new OrbitControls(camera, canvas);
+    controls.target.set(0, 5, 0);
+    controls.update();
+  
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color("black");
+  
+    {
+      const planeSize = 40;
+  
+      const loader = new THREE.TextureLoader();
+      const texture = loader.load(
+        "https://threejsfundamentals.org/threejs/resources/images/checker.png"
+      );
+      texture.wrapS = THREE.RepeatWrapping;
+      texture.wrapT = THREE.RepeatWrapping;
+      texture.magFilter = THREE.NearestFilter;
+      const repeats = planeSize / 2;
+      texture.repeat.set(repeats, repeats);
+  
+      const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
+      const planeMat = new THREE.MeshPhongMaterial({
+        map: texture,
+        side: THREE.DoubleSide
+      });
+      const mesh = new THREE.Mesh(planeGeo, planeMat);
+      mesh.rotation.x = Math.PI * -0.5;
+      scene.add(mesh);
+    }
+    {
+      const cubeSize = 4;
+      const cubeGeo = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
+      const cubeMat = new THREE.MeshPhongMaterial({ color: "#8AC" });
+      const mesh = new THREE.Mesh(cubeGeo, cubeMat);
+      mesh.position.set(cubeSize + 1, cubeSize / 2, 0);
+      scene.add(mesh);
+    }
+    {
+      const sphereRadius = 3;
+      const sphereWidthDivisions = 32;
+      const sphereHeightDivisions = 16;
+      const sphereGeo = new THREE.SphereGeometry(
+        sphereRadius,
+        sphereWidthDivisions,
+        sphereHeightDivisions
+      );
+      const sphereMat = new THREE.MeshPhongMaterial({ color: "#CA8" });
+      const mesh = new THREE.Mesh(sphereGeo, sphereMat);
+      mesh.position.set(-sphereRadius - 1, sphereRadius + 2, 0);
+      scene.add(mesh);
+    }
+  
+    {
+      const color = 0xffffff;
+      const intensity = 1;
+      const light = new THREE.DirectionalLight(color, intensity);
+      light.position.set(0, 10, 0);
+      light.target.position.set(-5, 0, 0);
+      scene.add(light);
+      scene.add(light.target);
+    }
+  
+    function resizeRendererToDisplaySize(renderer) {
+      const canvas = renderer.domElement;
+      const width = canvas.clientWidth;
+      const height = canvas.clientHeight;
+      const needResize = canvas.width !== width || canvas.height !== height;
+      if (needResize) {
+        renderer.setSize(width, height, false);
+      }
+      return needResize;
+    }
+  
+    function render() {
+      if (resizeRendererToDisplaySize(renderer)) {
+        const canvas = renderer.domElement;
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateProjectionMatrix();
+      }
+  
+      renderer.render(scene, camera);
+  
+      requestAnimationFrame(render);
+    }
+  
+    requestAnimationFrame(render);
+  }
+  
+  main();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var container3d = document.querySelector('.container3d');
+
+// var w = container3d.offsetWidth;
+// var h = container3d.offsetHeight;
+
+// var scene = new THREE.Scene();
+
+// var camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+// camera.position.z = 200;
+
+// var renderer = new THREE.WebGLRenderer({ antialias: true });
+// renderer.domElement.id = "canvasfirst";
+// renderer.domElement.classList.add("canvasfirst");
+// container3d.appendChild(renderer.domElement);
+// renderer.setSize(w, h);
+// renderer.setClearColor(0xefefef, 1);
+
+// var geometry = new THREE.TorusKnotGeometry(50, 6, 500, 16, 3, 2, 2);
+// var material = new THREE.MeshBasicMaterial({ color: 0x0000ff, wireframe: true }); // set the color of the material to blue (0x0000ff)
+// var cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
+
+// var light = new THREE.PointLight(0xFFFF00);
+// light.position.set(10, 10, 25);
+// scene.add(light);
+
+// var render = function () {
+//     requestAnimationFrame(render);
+
+//     cube.rotation.x += 0.01;
+//     cube.rotation.y += 0.005;
+//     cube.rotation.z += 0.05;
+
+//     renderer.render(scene, camera);
+// };
+// render();
+
+// window.addEventListener('resize', onWindowResize, false);
+
+// function onWindowResize() {
+//     w = container3d.offsetWidth;
+//     h = container3d.offsetHeight;
+//     camera.aspect = w / h;
+//     camera.updateProjectionMatrix();
+//     renderer.setSize(w, h);
+//     renderer.setClearColor(0xefefef, 1);
+// }
